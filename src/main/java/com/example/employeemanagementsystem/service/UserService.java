b0abf736a9bf5df02b5cc989e7c7af8025e3fb9e
TekendraKhatri7 <47274269+TekendraKhatri@users.noreply.github.com>
2024-09-05 09:27:14 +0100

    Delete src/main/java/com/example/employeemanagementsystem directory

--- src/main/java/com/example/employeemanagementsystem/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeemanagementsystem.service;
-
-import com.example.employeemanagementsystem.dto.UserRegistrationDto;
-import com.example.employeemanagementsystem.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto registrationDto);
-}
